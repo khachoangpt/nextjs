6d3c8a4d21dadd713783fcf63b5ea80a01ab48ed
Hoang Nguyen <hoang.nguyen@pionero.io>
2023-12-28 13:26:07 +0700

    fix: clean project

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,6 +1,7 @@
 module.exports = {
   // Type check TypeScript files
   '**/*.(ts|tsx)': () => 'npx tsc --noEmit',
+
   // Lint then format TypeScript and JavaScript files
   '**/*.(ts|tsx|js)': (filenames) => [
     `npx eslint --fix ${filenames.join(' ')}`,
